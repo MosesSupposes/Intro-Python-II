280b80137c132e6a22f9e6bd67661732ef172284
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-08 15:21:53 -0700

    create the game loop and provide a way to exit it

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import sys
 
 # Declare all the rooms
 
@@ -38,12 +40,21 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
+while(True):
+    print("You have entered the " + player.room.name)
+    print("Description: " + player.room.description)
+    next_room = input("Choose a direction to travel in [n, e, s, w]: ")
+
+    if next_room == "q": 
+        print("You have died of utter cowardice.")
+        sys.exit()
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.

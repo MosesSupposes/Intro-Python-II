54d55fb966a18d8c96b21b062eccf8e861903112
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-08 16:10:07 -0700

    refactor, add help prompt, add move_to method to player class

--- src/player.py
+++ src/player.py
@@ -1,5 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, room):
-        self.room = room
+    def __init__(self, starting_point):
+        self.room = starting_point 
+
+    def move_to(self, next_direction, all_rooms):
+        invalid_direction_msg = "Sorry, you can't go there."
+
+        if self.room == all_rooms['outside']:
+            if next_direction == "n":
+                self.room = all_rooms['outside'].n_to
+            else:
+                if (next_direction != "help"): 
+                    print(invalid_direction_msg) 
+                
